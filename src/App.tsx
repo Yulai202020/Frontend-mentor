9c0a44d4fa6b3ed4a85e34477a678bca34306cb0
Yulai202020 <ngmyulai@gmail.com>
2024-09-18 12:03:59 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import General from './components/General'
+import General from './components/social_links_profile/General'
+import { HashRouter as Router, Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <General />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/social_links_profile" element={<General />}/>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/social_links_profile/General.tsx
+++ src/components/social_links_profile/General.tsx
@@ -1,5 +1,5 @@
 import "./main.scss"
-import config from "../../config.json";
+import config from "../../../config.json";
 
 function General() {
     return (
