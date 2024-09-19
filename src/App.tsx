831389a79246102d0a40f1bc0c274000d5c5e517
Yulai202020 <ngmyulai@gmail.com>
2024-09-19 13:47:43 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+
 import SocialLinksProfile from './components/SocialLinksProfile/SocialLinksProfile'
 import BlogPreviewCard from './components/BlogPreviewCard/BlogPreviewCard';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />}/>
+
         <Route path="/SocialLinksProfile" element={<SocialLinksProfile />}/>
         <Route path="/BlogPreviewCard" element={<BlogPreviewCard />}/>
+
+        <Route path="*" element={<PageNotFound />}/>
       </Routes>
     </Router>
   );
