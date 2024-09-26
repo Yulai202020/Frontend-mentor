c616f62491cd0f1ff7a543bdd1fef634e720d9f8
Yulai202020 <ngmyulai@gmail.com>
2024-09-26 16:30:54 +0400

    first commit

--- src/components/InteractiveRating/InteractiveRating.tsx
+++ src/components/InteractiveRating/InteractiveRating.tsx
@@ -3,23 +3,23 @@ import config from "../../../config.json";
 import { useState } from "react";
 
 function InteractiveRating() {
-    const [rating, SetRating] = useState(0);
-    const [form_compilete, Setform_compilete] = useState(false);
+    const [rating, setRating] = useState(0);
+    const [formComplete, setFormComplete] = useState(false);
 
-    const handle_rate_buttons = (event: MouseEvent<HTMLButtonElement>) => {
+    const handleRateButtons = (event: React.MouseEvent<HTMLButtonElement>) => {
         const number = event.currentTarget.id;
-        SetRating(Number(number));
+        setRating(Number(number));
     }
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        Setform_compilete(true);
+        setFormComplete(true);
     }
 
     return (
         <div className={styles.container}>
             <form className={styles.form} onSubmit={onSubmit}>
-                {!form_compilete ? (
+                {!formComplete ? (
                     <>
                         <div className={styles.img_container}>
                             <img alt="star" className={styles.img_star} src={`${config.basepath}/star.svg`} />
@@ -34,10 +34,12 @@ function InteractiveRating() {
                         <p>
                             {[1, 2, 3, 4, 5].map(num => (
                                 <button
+                                    key={num} // Added key for list items
                                     id={num.toString()}
                                     type="button"
                                     className={`${styles.rating_buttons} ${rating === num ? styles.selected : ''}`}
-                                    onClick={handle_rate_buttons}
+                                    onClick={handleRateButtons}
+                                    aria-label={`Rate ${num}`} // Improved accessibility
                                 >
                                     {num}
                                 </button>
@@ -47,8 +49,8 @@ function InteractiveRating() {
                         <button type="submit" className={styles.submit_button}>Submit</button>
                     </>
                 ) : (
-                    <div className={styles.thank_contianer}>
-                        <img alt="lg" src={`${config.basepath}/thank-you.svg`} />
+                    <div className={styles.thank_container}>
+                        <img alt="Thank you" src={`${config.basepath}/thank-you.svg`} />
                         <p className={styles.rating}>You selected {rating} out of 5</p>
                         <h1 className={styles.thanks}>Thank you!</h1>
                         <p className={styles.other_text}>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
