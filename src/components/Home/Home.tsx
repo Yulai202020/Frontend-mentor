8ea40f09de430414b882d1f245a3028b2c8d9995
Yulai202020 <ngmyulai@gmail.com>
2024-10-16 08:51:03 +0400

    first commit

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,17 @@
+import style from "./style.module.scss";
+
 function Home() {
     return (
-        <h1>Home page.</h1>
+        <div className={style.box}>
+            <h1>Home page.</h1>
+
+            <h3>Links</h3>
+
+            <a className={style.link} href="/SocialLinksProfile">Social Links Profile</a>
+            <a className={style.link} href="/BlogPreviewCard">Blog Preview Card </a>
+            <a className={style.link} href="/Rating">Rating</a>
+            <a className={style.link} href="/QRCode">QR code</a>
+        </div>
     );
 }
 
--- src/components/QRCode/QRCode.tsx
+++ src/components/QRCode/QRCode.tsx
@@ -1,13 +1,13 @@
-import "./style.scss";
+import style from "./main.module.scss";
 
 function QRCode() {
     return (
         <>
-        <div className="container">
-            <div className="form">
-                <img alt="qr-code" className="qr-code-image" src="/image-qr-code.png" />
-                <h1 className="title">Improve your front-end skills by building projects</h1>
-                <p className="description">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        <div className={style.container}>
+            <div className={style.form}>
+                <img alt="qr_code" className={style.image_qr_code} src="/image-qr-code.png" />
+                <h1 className={style.title}>Improve your front-end skills by building projects</h1>
+                <p className={style.description}>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
             </div>
         </div>
         </>
